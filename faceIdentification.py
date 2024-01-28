8f1096286264b84d2c89127c811aef0997b6bcf0
Michael <michaelmiranda38@yahoo.com.br>
2024-01-28 18:21:55 -0300

    inclui App de detecção de rostos

--- faceIdentification.py
+++ faceIdentification.py
@@ -14,6 +14,9 @@ nomeDaJanela = "Detecção de Rosto"
 # Criar a janela de exibição
 cv2.namedWindow(nomeDaJanela, cv2.WINDOW_NORMAL)
 
+# Inicializar o contador de rostos
+contadorDeRostos =  0
+
 # Loop Principal
 while True:
     # Ler um quadro do vídeo
@@ -25,12 +28,16 @@ while True:
 
         # Iterar sobre os rostos detectados
         for face_location in face_locations:
+            # Incrementar o contador de rostos
+            contadorDeRostos += 1
+
             # Desenhar um retângulo ao redor do rosto
             top, right, bottom, left = face_location
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
 
-            # Exibir a mensagem "Rosto Detectado"
-            cv2.putText(frame, "Rosto Detectado", (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+            # Exibir a mensagem "Rosto Detectado" com o número do rosto
+            mensagem = f"Rosto Detectado {contadorDeRostos}"
+            cv2.putText(frame, mensagem, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 
         # Exibir o quadro resultante
         cv2.imshow(nomeDaJanela, frame)
@@ -38,8 +45,8 @@ while True:
     except Exception as e:
         print(f"Erro: {e}")
 
-    # Encerrar o programa ao pressionar a tecla 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # Encerrar o programa ao pressionar a tecla 's'
+    if cv2.waitKey(1) & 0xFF == ord('s'):
         break
 
 # Liberar recursos 
